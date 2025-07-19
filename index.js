15805c0928ef957693192d89bfc84abb97164a03
Brett Dixon <zarteastb@gmail.com>
2025-07-19 21:54:51 +0000

    c

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ import express from 'express';
 const app = express();
 const router = express.Router();
 router.get('/', (_req, res) => {
-    res.json({ message: 'Welcome to the Render Web
+    res.json({ message: 'Welcome to the Render Web Service!'
+});
 
 app.get('/', (_req, res) => {
     res.json({ message: 'Welcome to the Render Web Service!' });
