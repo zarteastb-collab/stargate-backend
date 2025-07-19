433b001c0f197097ad81167574b2249501d17f94
Brett Dixon <zarteastb@gmail.com>
2025-07-19 15:50:48 +0000

    c

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('index.js');
+const routes = require('./../stargate-backend/index.js'); // Adjust the path as necessary
 
 const app = express();
 const PORT = process.env.PORT || 3000;
